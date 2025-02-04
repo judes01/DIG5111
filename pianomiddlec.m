c4efee93d467d192d525458fdbd984964dfdd20e
Judah Ankrah <s22129511@bcud2-024473.students.uce.ac.uk>
2025-02-04 15:16:55 +0000

    adding test files

---
+++ pianomiddlec.m
@@ -0,0 +1,13 @@
+Filename = 'piano_middle_C.wav';
+% First read wav file into memory
+[Sig,Fs]=audioread(Filename);
+%Sig stores raw audio data in column;
+%Fs sampling frequency
+
+Duration = length(Sig)/Fs;
+disp(Duration);
+Ts = 1/Fs;
+Time = 0:Ts:Duration-Ts;
+plot(Time' ,Sig);
+ylabel('Amplitude');
+xlabel('Time Sec');
