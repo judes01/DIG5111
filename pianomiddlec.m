a17105aa1d26d0ed6c9ddee1357042fc8fa8f194
Judah Ankrah <s22129511@bcud2-024473.students.uce.ac.uk>
2025-02-05 14:30:43 +0000

    piano tutorial

--- pianomiddlec.m
+++ pianomiddlec.m
@@ -24,3 +24,23 @@ t2samples = t2*Fs;
 partSig = Sig(t1samples:t2samples);
 partTime = Time(t1samples:t2samples);
 plot(partTime ,partSig);
+
+
+Fs = 44100;   % Sampling rate (samples per second)
+Dur = 1;      % Duration in seconds
+
+% Create random noise for left and right channels
+Left_channel = randn(1, Fs * Dur);    % 1 second of random noise
+Right_channel = randn(1, Fs * Dur);   % 1 second of random noise
+
+% Combine into a stereo signal (2 rows, one for each channel)
+Stereo_Signal = [Left_channel; Right_channel]; 
+
+% Normalize the signal to the range [-1, 1]
+Stereo_Signal = Stereo_Signal / max(abs(Stereo_Signal(:)));  % Normalize
+
+% Define the filename for the output
+filename = 'middleCstereo.wav';  % Make sure this is a string
+
+% Write the stereo signal to a file
+audiowrite(filename, Stereo_Signal', Fs); % Transpose to 2 x N format
--- test.m
+++ test.m
@@ -1,1 +1,20 @@
 %test
+
+Fs = 44100;   % Sampling rate (samples per second)
+Dur = 1;      % Duration in seconds
+
+% Create random noise for left and right channels
+Left_channel = randn(1, Fs * Dur);    % 1 second of random noise
+Right_channel = randn(1, Fs * Dur);   % 1 second of random noise
+
+% Combine into a stereo signal (2 rows, one for each channel)
+Stereo_Signal = [Left_channel; Right_channel]; 
+
+% Normalize the signal to the range [-1, 1]
+Stereo_Signal = Stereo_Signal / max(abs(Stereo_Signal(:)));  % Normalize
+
+% Define the filename for the output
+filename = 'middleCstereo.wav';  % Make sure this is a string
+
+% Write the stereo signal to a file
+audiowrite(filename, Stereo_Signal', Fs); % Transpose
